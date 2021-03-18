43f48d54d641b20b52a69a5457e474ce588f3f4e
Adam Mish <mail@adammi.sh>
2021-03-17 18:35:16 -0700

    Adding basic styles for app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import logo from '/logo.png';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" align="center" gutterBottom>
-          Yardzen Budget Calculator
+    <Container maxWidth="md">
+      <Box my={4} p={4} bgcolor="white" boxShadow={1}>
+        <Box maxWidth={250} mx="auto" mb={1}>
+          <Link href="https://yardzen.com" display="block" align="center">
+            <img src="/logo.png" alt="Yardzen Logo" width="100%" />
+          </Link>
+        </Box>
+        <Typography variant="h4" component="h1" align="center">
+          Budget Calculator
         </Typography>
       </Box>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          backgroundColor: '#fafafa'
+        }
+      }
+    }
+  }
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     {/* CssBaseline adds a simple baseline to build upon. */}
     <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 

629cfc793c70f7bbdf68cbd51ed48ab43c81fe54
Adam Mish <mail@adammi.sh>
2021-03-20 20:43:17 -0700

    Fetch items from firebase, add budget input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import db from 'firebase.config.js';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Header from 'components/Header/Header';
+import Calculator from 'components/Calculator/Calculator';
+
+interface Item {
+  type: string;
+  name: string;
+  lowPrice: number;
+  highPrice: number;
+}
+
+interface Types {
+  [key: string]: Item[];
+}
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [types, setTypes] = useState<Types>({});
+  const [budget, setBudget] = useState<Number>(NaN);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    // Fetch items from firebase
+    const response = await db.collection('items').get();
+    let fetchedItems: Item[] = [];
+    response.docs.forEach((item) => {
+      fetchedItems.push(item.data() as Item);
+    });
+
+    setItems(fetchedItems);
+    setTypes(prepTypes(fetchedItems));
+  };
+
+  const prepTypes = (items: Item[]): Types => {
+    // Group items by type
+    const preparedTypes = groupBy(items, (i) => i.type);
+
+    Object.keys(preparedTypes).map((key, i) => {
+      // In each group, remove duplicate items
+      return preparedTypes[key].filter(
+        (v, i, a) => a.findIndex((t) => t.name === v.name) === i
+      );
+    });
+    return preparedTypes;
+  };
+
+  const handleBudgetChange = (budget: number) => {
+    setBudget(budget);
+  };
+
+  const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
+    list.reduce((previous, currentItem) => {
+      const group = getKey(currentItem);
+      if (!previous[group]) previous[group] = [];
+      previous[group].push(currentItem);
+      return previous;
+    }, {} as Record<K, T[]>);
+
   return (
     <Container maxWidth="md">
       <Box my={4} p={4} bgcolor="white" boxShadow={1}>
         <Header />
+        <Calculator
+          types={types}
+          budget={budget}
+          onBudgetChange={handleBudgetChange}
+        />
       </Box>
     </Container>
   );
---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+
+interface Props {
+  types: Object;
+  budget: Number;
+  onBudgetChange: (budget: number) => void;
+}
+
+function Calculator(props: Props) {
+  // Makes sure e.curentTarget.value is a number with parseFloat
+  const handleBudgetChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    props.onBudgetChange(parseFloat(e.currentTarget.value));
+  };
+
+  return (
+    <Box my={4}>
+      <Typography variant="h4" component="h1" align="center">
+        Calculator
+      </Typography>
+      {/* <Typography variant="body1">{JSON.stringify(props.types)}</Typography> */}
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <FormControl component="fieldset">
+            <InputLabel htmlFor="standard-adornment-budget">
+              Insert your budget
+            </InputLabel>
+            <Input
+              id="standard-adornment-budget"
+              type="number"
+              value={props.budget}
+              onChange={handleBudgetChange}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box mt={4}>
+            {props.budget ? (
+              <Typography variant="h5" component="h2">
+                Your budget: ${props.budget}
+              </Typography>
+            ) : (
+              <Typography variant="h5" component="h2">
+                Please input your budget
+              </Typography>
+            )}
+          </Box>
+          <Box mt={4}>
+            <Typography variant="h5" component="h2">
+              Select items range: <br /> $40000
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Calculator;
